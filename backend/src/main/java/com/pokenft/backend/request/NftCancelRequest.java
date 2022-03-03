eecba93efdfb9dcb9140cfe8e216529218fc67aa
Quentin Autret <autret.quentin@gmail.com>
2022-03-03 14:23:18 +0100

    cancel sale

---
+++ backend/src/main/java/com/pokenft/backend/request/NftCancelRequest.java
@@ -0,0 +1,28 @@
+package com.pokenft.backend.request;
+
+import javax.validation.constraints.NotNull;
+
+public class NftCancelRequest {
+
+	@NotNull
+	private long id;
+
+	@NotNull
+	private Double price;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
